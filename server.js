dc58b3e97987176d26c52e8a16baba59a5f5d2e3
nxhoang <nxhoang99@gmail.com>
2019-09-12 11:20:49 +0700

    a

--- server.js
+++ server.js
@@ -7,40 +7,43 @@ const usersRouter = require("./users/users.router");
 const postsRouter = require("./posts/posts.router");
 const uploadRouter = require("./upload/upload.router");
 const crawlRouter = require("./crawl/crawl.router");
-mongoose.connect("mongodb+srv://kutj_vip99:hoang123@mindx-fitness-tvkpd.mongodb.net/test?retryWrites=true&w=majority", error => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Connect to mongodb success");
-    const app = express();
+mongoose.connect(
+  "mongodb+srv://kutj_vip99:hoang123@mindx-fitness-tvkpd.mongodb.net/test?retryWrites=true&w=majority",
+  error => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Connect to mongodb success");
+      const app = express();
 
-    //cors
-    app.use(
-      cors({
-        origin: "https://mindx-fitness.herokuapp.com/",
-        credentials: true
-      })
-    );
-    app.use(express.static("public"));
-    app.options("*", cors());
-    app.use(bodyParser.json());
-    app.use(
-      session({
-        secret: "keyboard cat"
-      })
-    );
-    //router
-    app.use("/users", usersRouter);
-    app.use("/posts", postsRouter);
-    app.use("/upload", uploadRouter);
-    app.use("/find", crawlRouter);
-    //listen
-    app.listen(3001, error => {
-      if (error) {
-        throw error;
-      } else {
-        console.log("Server listen on port 3001...");
-      }
-    });
+      //cors
+      app.use(
+        cors({
+          origin: "https://mindx-fitness.herokuapp.com/",
+          credentials: true
+        })
+      );
+      app.use(express.static("public"));
+      app.options("*", cors());
+      app.use(bodyParser.json());
+      app.use(
+        session({
+          secret: "keyboard cat"
+        })
+      );
+      //router
+      app.use("/users", usersRouter);
+      app.use("/posts", postsRouter);
+      app.use("/upload", uploadRouter);
+      app.use("/find", crawlRouter);
+      //listen
+      app.listen(process.env.PORT || 3001, error => {
+        if (error) {
+          throw error;
+        } else {
+          console.log("Server listen on port 3001...");
+        }
+      });
+    }
   }
-});
+);
